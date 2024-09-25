c3003f624c16c19f62d5f71db8e341e33fb8492d
--replace <tejumoladavid@gmail.com>
2024-09-25 19:28:52 -0400

    implement join webhook event

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 import React from "react";
 import Image from "next/image";
-import { LayoutList, Bot, Mic, Check } from "lucide-react"
+import { LayoutList, Bot, Mic, Check, BriefcaseIcon, BriefcaseBusiness } from "lucide-react"
 import { Label } from "@/components/ui/label"
 import { Button } from "@/components/ui/button"
 import Webcam from "react-webcam";
@@ -28,35 +28,11 @@ interface UserSession {
     selected_role: string | null
 }
 
-type InterviewStyle = 'default' | 'video';
-
-const messages = [
-    {
-        sender: "Alex",
-        isCandidate: true,
-        text: "Hello, I am Alex. I am here for the interview.",
-    },
-    {
-        sender: "Interviewer",
-        isCandidate: false,
-        text: "Hello Alex, I am here to interview you.",
-    },
-    {
-        sender: "Alex",
-        isCandidate: true,
-        text: "I am ready for the interview.",
-    },
-    {
-        sender: "Interviewer",
-        isCandidate: false,
-        text: "Great! Let's start.",
-    },
-    {
-        sender: "Alex",
-        isCandidate: true,
-        text: "Sure.",
-    }
-]
+type Role = "assistant" | "candidate";
+interface Transcript {
+    role: Role;
+    text: string;
+}
 
 const videoConstraints = {
     width: 1880,
@@ -73,25 +49,66 @@ export default function Roles() {
     const [isAnswering, setIsAnswering] = React.useState(false);
     const initializedRef = React.useRef(false);
     const socketRef = React.useRef<Socket | null>(null);
-    const [transcripts, setTranscripts] = React.useState<string[]>([]);
+    const [transcripts, setTranscripts] = React.useState<Transcript[]>([]);
+
+    const initializeRoom = React.useCallback(async () => {
+        initializedRef.current = true;
+        const session_id = searchParams.get('session_id');
+
+        try {
+            if (socketRef.current?.connected) {
+                socketRef.current.emit('join', { session_id: session_id });
+            }
+        } catch (e) {
+            console.error('Error initializing room: ', e);
+        }
+    }, []);
 
     React.useEffect(() => {
-        const socket = io('http://0.0.0.0:8000', {
+        const socket = io(process.env.NEXT_PUBLIC_API_URL ??'http://0.0.0.0:8000', {
             transports: ["websocket"],
             reconnection: true,
         });
         socketRef.current = socket;
 
         socketRef.current.on('connect', () => {
+            // if(!initializedRef.current) {
+                initializeRoom();
+            // }
             console.log('Connected to server');
         });
 
-        return () => {
-            if (socketRef.current) {
-                socketRef.current.disconnect();
-            }
-        };
-    }, []);
+        socketRef.current.on('disconnect', () => {
+            console.log('Disconnected from server');
+        });
+
+        socketRef.current.on('chat', (data) => {
+            console.log('chat', data);
+            // setTranscripts(prevTranscripts => [...prevTranscripts, {
+            //     role: data.role as Role,
+            //     text: data.transcript
+            // }]);
+        });
+
+        // return () => {
+        //     if (socketRef.current) {
+        //         socketRef.current.disconnect();
+        //     }
+        // };
+    }, [initializeRoom]);
+
+    React.useEffect(() => {
+        if (!socketRef.current) return;
+        socketRef.current.on('chat', (data) => {
+            console.log('chat', data);
+            // setTranscripts(prevTranscripts => [...prevTranscripts, {
+            //     role: data.role as Role,
+            //     text: data.transcript
+            // }]);
+        });
+    }, [])
+
+
 
     const size = useWindowSize();
     if (!size.height || !size.width) {
@@ -112,23 +129,6 @@ export default function Roles() {
     //         ...userSession,
     //         selected_role: searchParams.get('role')
     //     });
-    // }, []);
-
-    // const initializeRoom = React.useCallback(async () => {
-    //     try {
-    //         if (initializedRef.current) return
-    //         console.log('Initializing room...');
-
-    //         if (socketRef.current) {
-    //             socketRef.current.emit('join', { code: interviewCode });
-
-    //             audioRef.current = new Audio("/assets/join_call.mp3");
-    //             await audioRef.current.play()
-    //         }
-
-    //     } catch (e) {
-    //         console.error('Error initializing room: ', e);
-    //     }
     // }, []);
 
    
@@ -160,10 +160,10 @@ export default function Roles() {
                     </div>
                     <div>
                         {
-                            messages.map((message, index) => (
+                            transcripts.map((message, index) => (
                                 <div key={index} className="flex flex-row">
                                     <div className="flex flex-col">
-                                        <p>{message.sender}</p>
+                                        <p>{message.role === "assistant" ? "Judith" : "You"}</p>
                                         <p>{message.text}</p>
                                     </div>
                                 </div>
@@ -174,7 +174,10 @@ export default function Roles() {
             </div>
 
             <div className="flex items-center h-20">
-                <h1 className="text-lg">Fullstack Interview Warmup</h1>
+                <div className="flex items-center space-x-2 opacity-70">
+                    <BriefcaseBusiness size={18} strokeWidth={2} />
+                    <h1 className="text-lg font-thin">Fullstack Interview Warmup</h1>
+                </div>
                 <Button
                     className="absolute space-x-1 flex justify-between items-center text-white px-[24px] py-[10px] rounded-sm left-[40%]"
                     onClick={() => setIsAnswering(!isAnswering)}
